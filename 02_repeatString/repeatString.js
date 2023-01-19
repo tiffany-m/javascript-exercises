71f8e1530f32d5c9c9684c7efbe1a8d9d867c8ae
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-01-19 13:54:24 -0500

    Complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+// Write a function that simply repeats the string a given number of times:
 
+// ex
+// repeatString('hey', 3) // returns 'heyheyhey'
+
+
+const repeatString = function(str, num) {
+    let newStr = "";
+
+    if(num === 0 || str === "") return "";
+    if(num < 0) return "ERROR";
+
+    for(let i = 1; i <= num; i++) {
+        newStr += str;
+    }
+
+    return newStr;
 };
 
 // Do not edit below this line

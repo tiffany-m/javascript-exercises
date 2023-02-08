9ae68fe4884e05b6cfc32b51a3cbc57946824ca0
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-02-08 16:39:39 -0500

    Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if(arr.length === 0) {
+    return 0;
+  } else if (arr.length === 1) {
+    return arr[0]
+  } else {
+    let total = arr.reduce((total, item) => total + item, 0)
+    return total;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length === 2) {
+    return arr[0] * arr[1];
+  } else {
+    let total = arr.reduce((total, item) => total * item, 1)
+    return total;
+  }
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a === 0) {
+    return 1;
+  } else {
+    let total = 1;
+    for(let i = 1; i <= a; i++) {
+      total = total * i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line

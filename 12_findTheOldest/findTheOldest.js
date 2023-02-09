9fd283e0164388940d534021c21323435a2ef3f1
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-02-09 11:45:07 -0500

    Fixes for findTheOldest, forgot to unskip tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
 const findTheOldest = function (obj) {
-    return array.reduce((oldest, currentPerson) => {
+    return obj.reduce((oldest, currentPerson) => {
         const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
         const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
         return oldestAge < currentAge ? currentPerson : oldest;

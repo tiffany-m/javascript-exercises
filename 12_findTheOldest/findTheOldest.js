22e72a9a03a4fdb21da82b83804ac0565c13f6bc
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-02-09 11:39:13 -0500

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,22 @@
 const findTheOldest = function (obj) {
-    let oldestPerosn = obj.reduce((oldest, currentPerson) => {
+    return array.reduce((oldest, currentPerson) => {
         const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
         const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
         return oldestAge < currentAge ? currentPerson : oldest;
-    }
-)
+    })
+};
 
-    function getAge(birth, death) {
-        if (!death) {
-            death = new Date().getFullYear();
-        }
-        return death - birth;
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
     }
-    
+    return death - birth;
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+
+
+// obj.reduce(function(accumulator/starting point, each item), starting point)
